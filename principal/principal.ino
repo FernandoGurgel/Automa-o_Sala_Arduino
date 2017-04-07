8b0062fec23a02aa1c15c3d501aaf668979c7cf2
Fernando Gurgel <fag.gurgel@gmail.com>
2017-04-07 16:57:33 -0400

    teste

--- principal/principal.ino
+++ principal/principal.ino
@@ -1,56 +1,69 @@
-int pinoAbriPorta = 12; 
-int pinoSensor = 11;
-int pinoPorta = 13;
-int pinoBlue = 10;
-int acionamento; 
-char buf;
-boolean ligado = false;
+#include "SoftwareSerial.h"
+
+SoftwareSerial bluetooth(3,2);
+
+int pinSenMovimento = 4; 
+int pinSisLigado = 5;
+int pinPorta = 6;
+boolean ligar = false;
+
+int movimento;
+int ON = LOW; 
+int OFF = HIGH;
  
 void setup(){
   
-  pinMode(pinoAbriPorta, OUTPUT); 
-  pinMode(pinoSensor, INPUT); 
-  pinMode(pinoPorta, OUTPUT);
-  pinMode(pinoBlue,OUTPUT);
+  pinMode(pinPorta, OUTPUT); 
+  pinMode(pinSenMovimento, INPUT); 
+  pinMode(pinSisLigado, OUTPUT);
   
   Serial.begin(9600);
+  bluetooth.begin(9600);
+  
 }
  
 void loop(){
-  buf = Serial.read();
-  
-  
-  if (buf == 'L' || ligado == true){
-    Serial.print(buf);
-    Serial.print("\n");
-    
-    digitalWrite(pinoBlue,HIGH);
-    ligado = true;
-    verificaPorta(ligado);
-  }else if ((buf == 'D' || ligado == false){
-    Serial.print(buf);
-    Serial.print("\n");
-    
-    digitalWrite(pinoBlue,LOW);
-    verificaPorta(ligado);
+ 
+  if(bluetooth.available()){
+    parametroBluetooth(bluetooth.read());
   }
-  
+
+  controlePorta(ligar);
+}
+
+void parametroBluetooth(char args){
+  switch(args){
+    case 'l':
+    case 'L':{
+        Serial.println("Bluetooth, ligado!!");
+        digitalWrite(pinSisLigado, ON);
+        ligar = true;
+        break;
+      }
+    case 'D':
+    case 'd':{
+      Serial.println("Bluetooth, dedsligado!!");
+      digitalWrite(pinSisLigado, OFF);
+      ligar = false;
+      break;
+    }
+  }
 }
 
 void verificaPorta(boolean args){
   if (args){
-     acionamento = digitalRead(pinoSensor);
-     
-     if (acionamento == LOW){
-        digitalWrite(pinoAbriPorta, LOW);
-        Serial.println("Parado");
-      }
-      else {
-        
-        digitalWrite(pinoAbriPorta, HIGH);
-        Serial.println("Movimento !!!");
-        
-      }
+    
+     movimento = digitalRead(pinSenMovimento);
+
+     if(movimento == ON){
+      Serial.println("Sensor Ligado, Porta Aberta!!");
+      digitalWrite(pinPorta, ON);
+     }else{
+      Serial.println("Sensor Ligado, Porta Desligado!!");
+      digitalWrite(pinPorta, OFF);
+     }
+  }else{
+    Serial.println("Sensor Movimento desligado!!");
   }
 }
 
