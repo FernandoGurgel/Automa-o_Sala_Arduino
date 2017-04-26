133a97280082b3a0bb9d4a73aab5a29dfb6b1b4d
Fernando Gurgel <fag.gurgel@gmail.com>
2017-04-26 17:52:18 -0400

    Init Ar condiionado

--- principal/principal.ino
+++ principal/principal.ino
@@ -2,20 +2,30 @@
 
 SoftwareSerial bluetooth(3,2);
 
+//porta digitral 
 int pinSenMovimento = 4; 
 int pinSisLigado = 5;
 int pinPorta = 6;
-boolean ligar = false;
+int pinAr = 7;
+int pinLampada = 8;
+int pinComputador = 9;
 
+//Variavel de controle
+boolean ligarSistema = false;
+boolean computador = false;
+boolean ar = false;
 int movimento;
-int ON = LOW; 
-int OFF = HIGH;
+int ON = HIGH; 
+int OFF = LOW;
  
 void setup(){
   
   pinMode(pinPorta, OUTPUT); 
   pinMode(pinSenMovimento, INPUT); 
   pinMode(pinSisLigado, OUTPUT);
+  pinMode(pinAr, OUTPUT);
+  pinMode(pinLampada, OUTPUT);
+  pinMode(pinComputador, OUTPUT);
   
   Serial.begin(9600);
   bluetooth.begin(9600);
@@ -26,29 +36,87 @@ void loop(){
  
   if(bluetooth.available()){
     parametroBluetooth(bluetooth.read());
+    
+  }else if (ligarSistema){
+    verificaPorta(ligarSistema);
+    sistemaLampada(ligarSistema);
+    sistemaComputador(computador);
+    sistemaAr(ar);
   }
-
-  controlePorta(ligar);
+  
 }
 
 void parametroBluetooth(char args){
   switch(args){
     case 'l':
     case 'L':{
-        Serial.println("Bluetooth, ligado!!");
+        Serial.println("Sistema ligado!!");
         digitalWrite(pinSisLigado, ON);
-        ligar = true;
+        ligarSistema = true;
         break;
       }
     case 'D':
     case 'd':{
-      Serial.println("Bluetooth, dedsligado!!");
+      Serial.println("Sistema desligado!!");
       digitalWrite(pinSisLigado, OFF);
-      ligar = false;
+      ligarSistema = false;
+      break;
+    }
+
+    case 'C':
+    case 'c':{
+      Serial.println("Computador ligado!!");
+      computador = true;
+      break;
+    }
+
+    case 'Z':
+    case 'z':{
+      Serial.println("Computador ligado!!");
+      computador = false;
+      break;
+    }
+
+    case 'a':
+    case 'A':{
+      Serial.println("Ar condicionado , liagado!!");
+      ar = true;
+      break;
+    }
+
+    case 's':
+    case 'S':{
+      Serial.println("Ar condicionado , Desligado!!");
+      ar = false;
       break;
     }
   }
 }
+
+void sistemaComputador(boolean args){
+  if(args){
+    digitalWrite(pinComputador,ON);
+  }else{
+    digitalWrite(pinComputador,OFF);
+  }
+}
+
+void sistemaLampada(boolean args){
+  if(args){
+    digitalWrite(pinLampada,ON);
+  }else{
+    digitalWrite(pinLampada,OFF);
+  }
+}
+
+void sistemaAr(boolean args){
+  if(args){
+    digitalWrite(pinAr,ON);
+  }else{
+    digitalWrite(pinAr,OFF);
+  }
+}
+
 
 void verificaPorta(boolean args){
   if (args){
